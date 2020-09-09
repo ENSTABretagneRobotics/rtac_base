96c6f3e6f0a06512c591baa1c6508d130ccfb218
Pierre Narvor <pierre.narvor@ensta-bretagne.fr>
2020-09-09 14:49:51 +0200

    Added setters for pose type

--- include/rtac_base/types/Pose.h
+++ include/rtac_base/types/Pose.h
@@ -4,6 +4,7 @@
 #include <iostream>
 
 #include <rtac_base/types/common.h>
+#include <rtac_base/algorithm.h>
 
 namespace rtac { namespace types {
 
@@ -20,6 +21,11 @@ class Pose
     
     Pose(const Vector3<T>&    translation = Vector3<T>(0,0,0),
          const Quaternion<T>& orientation = Quaternion<T>(1,0,0,0));
+    Pose(const Vector3<T>& translation, const Matrix3<T>& orientation);
+
+    void set_translation(const Vector3<T>& t);
+    void set_orientation(const Quaternion<T>& q);
+    void set_orientation(const Matrix3<T>& r);
 
     const Vector3<T>&    translation() const;
     const Quaternion<T>& orientation() const;
@@ -34,6 +40,32 @@ Pose<T>::Pose(const Vector3<T>& translation, const Quaternion<T>& orientation) :
     translation_(translation),
     orientation_(orientation)
 {}
+
+template <typename T>
+Pose<T>::Pose(const Vector3<T>& translation, const Matrix3<T>& orientation) :
+    translation_(translation),
+    orientation_()
+{
+    this->set_orientation(orientation);
+}
+
+template <typename T>
+void Pose<T>::set_translation(const Vector3<T>& t)
+{
+    translation_ = t;
+}
+
+template <typename T>
+void Pose<T>::set_orientation(const Quaternion<T>& q)
+{
+    orientation_ = q.normalized();
+}
+
+template <typename T>
+void Pose<T>::set_orientation(const Matrix3<T>& r)
+{
+    orientation_ = rtac::algorithm::orthonormalized(orientation);
+}
 
 template <typename T>
 const Vector3<T>& Pose<T>::translation() const
