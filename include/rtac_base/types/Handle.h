975bf5a4b7fd6eb0cb94a6c7898bcfa6708b30f3
Pierre Narvor <pierre.narvor@ensta-bretagne.fr>
2020-10-30 12:16:39 +0100

    [types] Added a common Handle type for rtac (alias for std::shared_ptr)

---
+++ include/rtac_base/types/Handle.h
@@ -0,0 +1,17 @@
+#ifndef _DEF_RTAC_BASE_TYPES_HANDLE_H_
+#define _DEF_RTAC_BASE_TYPES_HANDLE_H_
+
+#include <memory>
+
+namespace rtac { namespace types {
+
+// This is defined here to be able to change it depending on target system (or
+// falling back on boost library if needed).
+template <typename T>
+using Handle = std::shared_ptr<T>;
+
+}; //namespace types
+}; //namespace rtac
+
+#endif //_DEF_RTAC_BASE_TYPES_HANDLE_H_
+
