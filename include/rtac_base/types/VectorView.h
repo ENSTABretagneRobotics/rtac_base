e03468b06adca8dfc4fb155ec5c78d75f8bfe45f
Pierre Narvor <pierre.narvor@ensta-bretagne.fr>
2022-07-01 17:46:13 +0200

    [types] Simplified VectorView (removed some compile time checks)

--- include/rtac_base/types/VectorView.h
+++ include/rtac_base/types/VectorView.h
@@ -7,86 +7,8 @@
 
 namespace rtac { namespace types {
 
-namespace details {
-
-// /**
-//  * These implements operator[] in a class only if VectorT also implements it.
-//  * 
-//  * Useful for DeviceVector where operator[] is defined only on device side.
-//  */
-// template <class Derived, typename T, bool IsSubscriptable>
-// struct subscript_traits {};
-
-template <class Derived>
-struct const_subscriptable_traits
-{
-    private:
-    // this is for convenience.
-    RTAC_HOSTDEVICE const Derived* derived() const {
-        return reinterpret_cast<const Derived*>(this);
-    }
-
-    public:
-
-    RTAC_HOSTDEVICE const auto& operator[](std::size_t idx) const {
-        return this->derived()->data()[idx];
-    }
-
-    RTAC_HOSTDEVICE const auto& front() const { return (*this)[0]; }
-    RTAC_HOSTDEVICE const auto& back() const  { return (*this)[this->derived()->size() - 1]; }
-};
-
-template <class Derived>
-struct mutable_subscriptable_traits
-{
-    private:
-    // this is for convenience.
-    RTAC_HOSTDEVICE Derived* derived() {
-        return reinterpret_cast<Derived*>(this);
-    }
-    RTAC_HOSTDEVICE const Derived* derived() const {
-        return reinterpret_cast<const Derived*>(this);
-    }
-
-    public:
-
-    RTAC_HOSTDEVICE auto& operator[](std::size_t idx) { 
-        return this->derived()->data()[idx];
-    }
-    RTAC_HOSTDEVICE auto& front() { return (*this)[0]; }
-    RTAC_HOSTDEVICE auto& back()  { return (*this)[this->derived()->size() - 1]; }
-
-    RTAC_HOSTDEVICE const auto& operator[](std::size_t idx) const {
-        return this->derived()->data()[idx];
-    }
-    RTAC_HOSTDEVICE const auto& front() const { return (*this)[0]; }
-    RTAC_HOSTDEVICE const auto& back() const  { return (*this)[this->derived()->size() - 1]; }
-};
-
-
-};
-
-template <class Derived, typename T, bool Subscriptable>
-struct subscriptable_traits {};
-
-template <class Derived, typename T>
-struct subscriptable_traits<Derived, T, true> :
-    public details::mutable_subscriptable_traits<Derived>
-{};
-
-template <class Derived, typename T>
-struct subscriptable_traits<Derived, const T, true> :
-    public details::const_subscriptable_traits<Derived>
-{};
-
-/**
- * Generic VectorView type. Mutable by default. Immutable is implemented as a
- * specialization and removes modifiers.
- *
- * The subscript_traits add the operator[] only if it is defined for VectorT.
- */
-template <typename T, bool Subscriptable = true>
-class VectorView : public subscriptable_traits<VectorView<T>, T, Subscriptable>
+template <typename T>
+class VectorView
 {
     public:
     
@@ -100,6 +22,8 @@ class VectorView : public subscriptable_traits<VectorView<T>, T, Subscriptable>
     public:
 
     VectorView(std::size_t size = 0, T* data = nullptr) : data_(data), size_(size) {}
+    //template <template<typename>class VectorT>
+    //VectorView(VectorT<T>& vector) : data_(vector.data()), size_(vector.size()) {}
 
     RTAC_HOSTDEVICE std::size_t size() const { return size_; }
 
@@ -110,14 +34,22 @@ class VectorView : public subscriptable_traits<VectorView<T>, T, Subscriptable>
     RTAC_HOSTDEVICE value_type* data()  { return data_; }
     RTAC_HOSTDEVICE value_type* begin() { return data_; }
     RTAC_HOSTDEVICE value_type* end()   { return data_ + size_; }
+    
+    RTAC_HOSTDEVICE const T& operator[](std::size_t idx) const { return data_[idx]; }
+    RTAC_HOSTDEVICE const T& front()                     const { return data_[0]; }
+    RTAC_HOSTDEVICE const T& back()                      const { return data_[size_ - 1]; }
+    
+    // modifiers
+    RTAC_HOSTDEVICE T& operator[](std::size_t idx) { return data_[idx]; }
+    RTAC_HOSTDEVICE T& front()                     { return data_[0]; }
+    RTAC_HOSTDEVICE T& back()                      { return data_[size_ - 1]; }
 };
 
 /**
  * Specialization of VectorView to handle const Vectors. Modifiers are removed.
  */
-template <typename T, bool Subscriptable>
-class VectorView<const T, Subscriptable>
-    : public subscriptable_traits<VectorView<const T>, const T, Subscriptable>
+template <typename T>
+class VectorView<const T>
 {
     public:
     
@@ -131,26 +63,31 @@ class VectorView<const T, Subscriptable>
     public:
 
     VectorView(std::size_t size = 0, const T* data = nullptr) : data_(data), size_(size) {}
+    //template <template<typename>class VectorT>
+    //VectorView(const VectorT<T>& vector) : data_(vector.data()), size_(vector.size()) {}
 
     RTAC_HOSTDEVICE std::size_t size() const { return size_; }
 
     RTAC_HOSTDEVICE const value_type* data()  const { return data_; }
     RTAC_HOSTDEVICE const value_type* begin() const { return data_; }
     RTAC_HOSTDEVICE const value_type* end()   const { return data_ + size_; }
+
+    RTAC_HOSTDEVICE const T& operator[](std::size_t idx) const { return data_[idx]; }
+    RTAC_HOSTDEVICE const T& front()                     const { return data_[0]; }
+    RTAC_HOSTDEVICE const T& back()                      const { return data_[size_ - 1]; }
 };
 
-template <typename VectorT>
-auto make_view(const VectorT& vector)
-{
-    return VectorView<const typename VectorT::value_type,
-                      is_subscriptable<VectorT>::value>(vector.size(), vector.data());
-}
-
-template <typename VectorT>
+// have to define these proxy types. nvcc does not play well with templates...
+template <class VectorT>
 auto make_view(VectorT& vector)
 {
-    return VectorView<typename VectorT::value_type,
-                      is_subscriptable<VectorT>::value>(vector.size(), vector.data());
+    return VectorView(vector.size(), vector.data());
+}
+
+template <class VectorT>
+auto make_view(const VectorT& vector)
+{
+    return VectorView(vector.size(), vector.data());
 }
 
 }; //namespace types
