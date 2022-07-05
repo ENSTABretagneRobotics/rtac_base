198eb7ec89cc68d34d52743a8eb5e41aef944261
Pierre Narvor <pierre.narvor@ensta-bretagne.fr>
2022-07-05 13:16:25 +0200

    Added new define in cuda_define for cuda kernel specific code

--- include/rtac_base/cuda_defines.h
+++ include/rtac_base/cuda_defines.h
@@ -3,11 +3,19 @@
 
 #if defined(__CUDACC__) || defined(__CUDABE__)
 #   define RTAC_CUDACC
+#   define RTAC_HOST       __host__
+#   define RTAC_DEVICE     __device__
 #   define RTAC_HOSTDEVICE __host__ __device__
 #   define RTAC_INLINE     __forceinline__
 #else
+#   define RTAC_HOST
+#   define RTAC_DEVICE
 #   define RTAC_HOSTDEVICE
 #   define RTAC_INLINE     inline
+#endif
+
+#if defined(__CUDA_ARCH__)
+#   define RTAC_KERNEL  // this is defined only for cuda kernel code
 #endif
 
 #endif //_DEF_RTAC_BASE_CUDA_DEFINES_H_
