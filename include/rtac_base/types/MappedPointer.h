c85b76961d381f4b19d7986d73296accc320d53a
Pierre Narvor <pierre.narvor@ensta-bretagne.fr>
2022-11-21 12:53:07 +0100

    [types] Better mapped pointer implementation

--- include/rtac_base/types/MappedPointer.h
+++ include/rtac_base/types/MappedPointer.h
@@ -1,65 +1,132 @@
 #ifndef _DEF_RTAC_BASE_TYPES_MAPPED_POINTER_H_
 #define _DEF_RTAC_BASE_TYPES_MAPPED_POINTER_H_
 
+#include <memory>
+
 #include <rtac_base/types/Handle.h>
 
 namespace rtac { namespace types {
 
-// This type will automatically grab a resource when created and release the
-// resource when going out of scope.
+template <class VectorT>
+class MappedPointerImpl
+{
+    public:
 
-template <typename MappedT,
-          typename PointerT = typename std::conditional<std::is_const<MappedT>::value,
-                                const typename MappedT::value_type*,
-                                typename MappedT::value_type*>::type>
+    using value_type      = typename VectorT::value_type;
+    using MapMethodType   = value_type*(VectorT::*)(void);
+    using UnmapMethodType = void(VectorT::*)(void) const;
+
+    protected:
+
+    VectorT*        vector_;
+    value_type*     ptr_;
+    UnmapMethodType unmapMethod_;
+
+    public:
+
+    MappedPointerImpl(VectorT* vector,
+                      MapMethodType   mapMethod,
+                      UnmapMethodType unmapMethod) :
+        vector_(vector),
+        ptr_((vector->*mapMethod)()),
+        unmapMethod_(unmapMethod)
+    {}
+    ~MappedPointerImpl() { this->unmap(); }
+
+    void unmap()
+    {
+        (vector_->*unmapMethod_)();
+        vector_ = nullptr;
+    }
+    value_type*       ptr()       { return ptr_; }
+    const value_type* ptr() const { return ptr_; }
+};
+
+template <typename VectorT>
 class MappedPointer
 {
     public:
 
-    using MappedType    = MappedT;
-    using PointerType   = PointerT;
-    using MapMethodType = typename std::conditional<std::is_const<MappedType>::value,
-                                                    PointerType(MappedT::*)(void) const,
-                                                    PointerType(MappedT::*)(void)>::type;
-    using UnmapMethodType = void(MappedT::*)(void) const;
+    using value_type      = typename VectorT::value_type;
+    using MapMethodType   = value_type*(VectorT::*)(void);
+    using UnmapMethodType = void(VectorT::*)(void) const;
 
     protected:
 
-    MappedType* mappedObject_;
-    PointerType ptr_;
-    UnmapMethodType unmapping_method_;
-    
+    std::shared_ptr<MappedPointerImpl<VectorT>> ptr_;
+
     public:
 
-    MappedPointer(MappedType* object,
-                  MapMethodType   mapping_method,
-                  UnmapMethodType unmapping_method) :
-        mappedObject_(object),
-        ptr_((object->*mapping_method)()),
-        unmapping_method_(unmapping_method)
+    MappedPointer(VectorT*        vector,
+                  MapMethodType   mapMethod,
+                  UnmapMethodType unmapMethod) :
+        ptr_(std::make_shared<MappedPointerImpl<VectorT>>(vector, mapMethod, unmapMethod))
     {}
 
-    // disabling copying, // reenabled because cuda
-    //MappedPointer(const MappedPointer& other)            = delete;
-    //MappedPointer& operator=(const MappedPointer& other) = delete;
+    operator       value_type*()       { return ptr_->ptr(); }
+    operator const value_type*() const { return ptr_->ptr(); }
 
-    ~MappedPointer()
-    {
-        // MappedResource will be released when this object goes out of scope.
-        this->unmap();
-    }
+    value_type&       operator[](std::size_t idx)       { return ptr_->ptr()[idx]; }
+    const value_type& operator[](std::size_t idx) const { return ptr_->ptr()[idx]; }
+};
+
+template <class VectorT>
+class MappedPointerImpl<const VectorT>
+{
+    public:
+
+    using value_type      = typename VectorT::value_type;
+    using MapMethodType   = const value_type*(VectorT::*)(void) const;
+    using UnmapMethodType = void(VectorT::*)(void) const;
+
+    protected:
+
+    const VectorT*    vector_;
+    const value_type* ptr_;
+    UnmapMethodType   unmapMethod_;
+
+    public:
+
+    MappedPointerImpl(const VectorT*  vector,
+                      MapMethodType   mapMethod,
+                      UnmapMethodType unmapMethod) :
+        vector_(vector),
+        ptr_((vector->*mapMethod)()),
+        unmapMethod_(unmapMethod)
+    {}
+    ~MappedPointerImpl() { this->unmap(); }
 
     void unmap()
     {
-        (mappedObject_->*unmapping_method_)();
-        mappedObject_ = nullptr;
+        (vector_->*unmapMethod_)();
+        vector_ = nullptr;
     }
+    const value_type* ptr() { return ptr_; }
+};
 
-    // Implicit cast to mapped pointer.
-    operator PointerType()
-    {
-        return ptr_;
-    }
+template <typename VectorT>
+class MappedPointer<const VectorT>
+{
+    public:
+
+    using value_type      = typename VectorT::value_type;
+    using MapMethodType   = const value_type*(VectorT::*)(void) const;
+    using UnmapMethodType = void(VectorT::*)(void) const;
+
+    protected:
+
+    std::shared_ptr<MappedPointerImpl<const VectorT>> ptr_;
+
+    public:
+
+    MappedPointer(const VectorT*  vector,
+                  MapMethodType   mapMethod,
+                  UnmapMethodType unmapMethod) :
+        ptr_(std::make_shared<MappedPointerImpl<const VectorT>>(vector, mapMethod, unmapMethod))
+    {}
+
+    operator const value_type*() const { return ptr_->ptr(); }
+    const value_type& operator[](std::size_t idx) const { return ptr_->ptr()[idx]; }
 };
 
 }; //namespace types
