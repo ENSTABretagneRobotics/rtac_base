9fe5cec17b8f28ee1f38030d3eb174100e58d499
Pierre Narvor <pierre.narvor@ensta-bretagne.fr>
2023-01-10 17:02:57 +0100

    [docs] Documentation more automated through the use of rtac_build_docs

--- cmake/rtac_build_docs.cmake
+++ cmake/rtac_build_docs.cmake
@@ -1,6 +1,8 @@
 
 
-function(target_add_docs TARGET_NAME)
+set(RTAC_BUILD_DOCS_DIR ${CMAKE_CURRENT_LIST_DIR})
+
+function(rtac_build_docs TARGET_NAME)
     find_package(Doxygen REQUIRED)
 
     if(NOT DOXYGEN_FOUND)
@@ -11,11 +13,14 @@ function(target_add_docs TARGET_NAME)
 
     get_target_property(DOXYGEN_EXECUTABLE Doxygen::doxygen IMPORTED_LOCATION)
     message(STATUS "DOXYGEN_EXECUTABLE : ${DOXYGEN_EXECUTABLE}")
+    
+    set(DOXYFILE_PATH ${CMAKE_CURRENT_SOURCE_DIR}/docs/Doxyfile)
+    configure_file(${RTAC_BUILD_DOCS_DIR}/Doxyfile.in ${DOXYFILE_PATH} @ONLY)
 
-    # Genewrating documentation in PRE_BUILD mode so the documentation is ready
+    # Generating documentation in PRE_BUILD mode so the documentation is ready
     # if we have an issue with the build of TARGET_NAME itself.
     add_custom_command(TARGET ${TARGET_NAME} PRE_BUILD  
-                       COMMAND ${DOXYGEN_EXECUTABLE} ${CMAKE_CURRENT_SOURCE_DIR}/docs/Doxyfile
+                       COMMAND ${DOXYGEN_EXECUTABLE} ${DOXYFILE_PATH}
                        WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
                        COMMENT "Building documentation for ${TARGET_NAME}"
                        VERBATIM USES_TERMINAL)
