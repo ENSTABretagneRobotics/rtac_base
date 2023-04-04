f9aa34e503bd386df5985e72dd6cee7399547011
Pierre Narvor <pierre.narvor@ensta-bretagne.fr>
2023-04-04 14:20:15 +0200

    [external] Added component parser in metashape parser (component = global transformation on computed poses)

--- include/rtac_base/external/metashape.h
+++ include/rtac_base/external/metashape.h
@@ -5,6 +5,7 @@
 #include <vector>
 #include <map>
 #include <string>
+#include <deque>
 
 #include <rtac_base/types/Pose.h>
 
@@ -23,13 +24,42 @@ class MetashapeOutput
         std::string label;
     };
 
-    using PoseContainer = std::map<int, std::vector<PoseInfo>>;
+    struct Component
+    {
+        using Vec3 = Pose<float>::Vec3;
+        using Mat3 = Pose<float>::Mat3;
+
+        int id;
+        std::string label;
+
+        Pose<float> transform;
+        float       scale;
+        
+        Vec3 regionCenter;
+        Vec3 regionSize;
+        Mat3 regionRot;
+
+        Component() :
+            id(0), label("NoLabel"), scale(1.0f),
+            regionCenter({0,0,0}),
+            regionSize({1,1,1}),
+            regionRot(Mat3::Identity())
+        {}
+    };
+
+    using ComponentContainer = std::map<int, Component>;
+    using PoseInfoContainer  = std::map<int, std::vector<PoseInfo>>;
+    using PoseContainer      = std::map<int, std::vector<Pose<float>>>;
 
     protected:
-
+    
     std::string filename_;
 
+    ComponentContainer components_;
+
     unsigned int poseCount_;
+    PoseInfoContainer poseInfo_;
+
     PoseContainer poses_;
 
     public:
@@ -37,14 +67,22 @@ class MetashapeOutput
     MetashapeOutput() {}
 
     void load_file(const std::string& filename);
-    void clear() { filename_ = ""; poses_.clear(); }
+    void clear() { filename_ = ""; poseInfo_.clear(); }
 
-    const PoseContainer& poses()                  const { return poses_; }
-    const std::vector<PoseInfo>& poses(int camId) const { return poses_.at(camId); }
+    const ComponentContainer& components() const { return components_; }
+    const Component& component(int id) const { return components_.at(id); }
+
+    const PoseInfoContainer&     pose_info()          const { return poseInfo_; }
+    const std::vector<PoseInfo>& pose_info(int camId) const { return poseInfo_.at(camId); }
+
+    const PoseContainer&            poses()          const { return poses_; }
+    const std::vector<Pose<float>>& poses(int camId) const { return poses_.at(camId); }
+
 };
-
 
 } //namespace external
 } //namespace rtac
+
+std::ostream& operator<<(std::ostream& os, const rtac::external::MetashapeOutput::Component& cmp);
 
 #endif //_DEF_RTAC_BASE_EXTERNAL_METASHAPE_H_
--- src/external/metashape.cpp
+++ src/external/metashape.cpp
@@ -1,6 +1,7 @@
 #include <rtac_base/external/metashape.h>
 
 #include <sstream>
+#include <string>
 
 #include <rtac_base/external/tinyxml2.h>
 
@@ -66,6 +67,126 @@ MetashapeOutput::PoseInfo parse_pose(XMLElement* cam)
     return info;
 }
 
+MetashapeOutput::Component parse_component(XMLElement* element)
+{
+    MetashapeOutput::Component component;
+
+    if(XML_SUCCESS != element->QueryIntAttribute("id", &component.id)) {
+        throw std::runtime_error("No id attribute in component element");
+    }
+    if(auto str = element->Attribute("label")) {
+        component.label = str;
+    }
+
+    if(auto tr = element->FirstChildElement("transform")) {
+        if(auto rot = tr->FirstChildElement("rotation")) {
+            if(auto rotStr = rot->GetText()) {
+                std::string data(rotStr);
+                std::istringstream iss(data);
+                std::string token;
+                Pose<float>::Mat3 mat;
+                for(int r = 0; r < 3; r++) { for(int c = 0; c < 3; c++) {
+                    if(!std::getline(iss, token, ' ')) {
+                        throw std::runtime_error("Invalid transform R format");
+                    }
+                    mat(r,c) = std::stof(token);
+                }}
+                component.transform.set_orientation(mat);
+            }
+        }
+        if(auto t = tr->FirstChildElement("translation")) {
+            if(auto tStr = t->GetText()) {
+                std::string data(tStr);
+                std::istringstream iss(data);
+                std::string token;
+                Pose<float>::Vec3 vec;
+                for(int r = 0; r < 3; r++) {
+                    if(!std::getline(iss, token, ' ')) {
+                        throw std::runtime_error("Invalid transform T format");
+                    }
+                    vec(r) = std::stof(token);
+                }
+                component.transform.set_translation(vec);
+            }
+        }
+        if(auto scale = tr->FirstChildElement("scale")) {
+            if(auto tStr = scale->GetText()) {
+                component.scale = std::stof(tStr);
+            }
+        }
+    }
+
+    if(auto reg = element->FirstChildElement("region")) {
+        if(auto center = reg->FirstChildElement("center")) {
+            if(auto cStr = center->GetText()) {
+                std::string data(cStr);
+                std::istringstream iss(data);
+                std::string token;
+                Pose<float>::Vec3 vec;
+                for(int r = 0; r < 3; r++) {
+                    if(!std::getline(iss, token, ' ')) {
+                        throw std::runtime_error("Invalid region center format");
+                    }
+                    vec(r) = std::stof(token);
+                }
+                component.regionCenter = vec;
+            }
+        }
+        if(auto size = reg->FirstChildElement("size")) {
+            if(auto sStr = size->GetText()) {
+                std::string data(sStr);
+                std::istringstream iss(data);
+                std::string token;
+                Pose<float>::Vec3 vec;
+                for(int r = 0; r < 3; r++) {
+                    if(!std::getline(iss, token, ' ')) {
+                        throw std::runtime_error("Invalid region size format");
+                    }
+                    vec(r) = std::stof(token);
+                }
+                component.regionSize = vec;
+            }
+        }
+        if(auto rot = reg->FirstChildElement("R")) {
+            if(auto rotStr = rot->GetText()) {
+                std::string data(rotStr);
+                std::istringstream iss(data);
+                std::string token;
+                Pose<float>::Mat3 mat;
+                for(int r = 0; r < 3; r++) { for(int c = 0; c < 3; c++) {
+                    if(!std::getline(iss, token, ' ')) {
+                        throw std::runtime_error("Invalid region R format");
+                    }
+                    mat(r,c) = std::stof(token);
+                }}
+                component.regionRot = mat;
+            }
+        }
+    }
+
+    return component;
+}
+
+std::deque<MetashapeOutput::Component> 
+    parse_components(XMLElement* chunk)
+{
+    std::deque<MetashapeOutput::Component> components;
+
+    auto element = chunk->FirstChildElement("components");
+    if(!element) {
+        std::cerr << "No 'components' element in metashape pose file" << std::endl;
+        return components;
+    }
+
+    auto compElement = element->FirstChildElement("component");
+    while(compElement) {
+        components.push_back(parse_component(compElement));
+        compElement = compElement->NextSiblingElement("component");
+    }
+
+    return components;
+}
+
 void MetashapeOutput::load_file(const std::string& filename)
 {
     filename_ = filename;
@@ -88,6 +209,11 @@ void MetashapeOutput::load_file(const std::string& filename)
         oss << "XML : no chunk element in document element"
             << "\n    file : " << filename_;
         throw std::runtime_error(oss.str());
+    }
+
+    auto components = parse_components(element);
+    for(const auto& cmp : components) {
+        components_[cmp.id] = cmp;
     }
 
     element = element->FirstChildElement("cameras");
@@ -118,11 +244,23 @@ void MetashapeOutput::load_file(const std::string& filename)
 
         while(cam) {
             auto info = parse_pose(cam);
-            poses_[info.sensorId].push_back(info);
+            poseInfo_[info.sensorId].push_back(info);
             cam = cam->NextSiblingElement("camera");
             parsed++;
         }
         group = group->NextSiblingElement("group");
+    }
+    
+    // Generating poses_ (compensate for metashape component transformation)
+    for(const auto& info : poseInfo_) {
+        poses_[info.first] = std::vector<Pose<float>>();
+        auto it = poses_.find(info.first);
+        for(const auto& pinfo : info.second) {
+            auto p = pinfo.pose;
+            const auto& cmp = components_.at(pinfo.componentId);
+            p.translation() *= cmp.scale;
+            it->second.push_back(cmp.transform*p);
+        }
     }
 
     std::cout << "Parsed " << parsed << " camera elements." << std::endl;
@@ -131,3 +269,33 @@ void MetashapeOutput::load_file(const std::string& filename)
 } //namespace external
 } //namespace rtac
 
+std::ostream& operator<<(std::ostream& os, const rtac::external::MetashapeOutput::Component& cmp)
+{
+    os << "Metashape component (id : " << cmp.id << ", label : " << cmp.label << ") :\n";
+    os << "- transform : (scaling = " << cmp.scale << ")\n";
+    {
+        std::ostringstream oss;
+        oss << cmp.transform.homogeneous_matrix();
+        std::istringstream iss(oss.str());
+        std::string token;
+        while(std::getline(iss, token)) {
+            os << "    " << token << "\n";
+        }
+    }
+    os << "- region :\n"
+       << "  - center   : " << cmp.regionCenter.transpose() << "\n"
+       << "  - size     : " << cmp.regionSize.transpose()   << "\n"
+       << "  - rotation :";
+    {
+        std::ostringstream oss;
+        oss << cmp.regionRot;
+        std::istringstream iss(oss.str());
+        std::string token;
+        std::getline(iss, token);
+        os << token << "\n";
+        while(std::getline(iss, token)) {
+            os << "              " << token << "\n";
+        }
+    }
+    return os;
+}
