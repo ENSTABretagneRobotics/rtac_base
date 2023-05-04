5cdf8a2fc935bf76b4b2231ccd7168ad34781163
Pierre Narvor <pierre.narvor@ensta-bretagne.fr>
2023-05-04 12:10:09 +0200

    [serialization] Added deserilaization utilities

--- include/rtac_base/serialization/serialization.h
+++ include/rtac_base/serialization/serialization.h
@@ -14,9 +14,13 @@ namespace rtac {
 struct ChunkHeader32;
 struct ChunkHeader64;
 struct SerializeStub;
+struct DeserializeStub32;
+struct DeserializeStub64;
 } 
-inline rtac::SerializeStub operator<<(std::ostream& os, const rtac::ChunkHeader32& chunkHeader);
-inline rtac::SerializeStub operator<<(std::ostream& os, const rtac::ChunkHeader64& chunkHeader);
+inline rtac::SerializeStub     operator<<(std::ostream&, const rtac::ChunkHeader32&);
+inline rtac::SerializeStub     operator<<(std::ostream&, const rtac::ChunkHeader64&);
+inline rtac::DeserializeStub32 operator>>(std::istream&, rtac::ChunkHeader32&);
+inline rtac::DeserializeStub64 operator>>(std::istream&, rtac::ChunkHeader64&);
 
 namespace rtac {
 
@@ -42,6 +46,7 @@ struct ChunkHeader32
 
     ChunkHeader32(const StringId<4>& i, uint32_t s) : id(i), size(s) {}
     ChunkHeader32(const std::string& i, uint32_t s) : id(i), size(s) {}
+    static ChunkHeader32 Empty() { return ChunkHeader32("UNKN", 0); }
     
     // saved size is aligned on 32bits
     uint32_t written_size() const {
@@ -63,9 +68,10 @@ struct ChunkHeader64
     StringId<8> id;
     uint64_t    size;
 
-    ChunkHeader64()                              = default;
+    ChunkHeader64()                                = default;
     ChunkHeader64(const ChunkHeader64&)            = default;
     ChunkHeader64& operator=(const ChunkHeader64&) = default;
+    static ChunkHeader64 Empty() { return ChunkHeader64("UNKN", 0); }
 
     ChunkHeader64(const StringId<8>& i, uint64_t s) : id(i), size(s) {}
     ChunkHeader64(const std::string& i, uint64_t s) : id(i), size(s) {}
@@ -126,6 +132,85 @@ class SerializeStub
     }
 };
 
+/**
+ * This is a temporary type automatically instanciated when using operator>> on
+ * a std::ostream and an empty ChunkHeader.
+ */
+class DeserializeStub32
+{
+    public:
+    
+    //restricting this object instanciation to this function
+    friend DeserializeStub32 (::operator>>)(std::istream&, rtac::ChunkHeader32&);
+
+    protected:
+
+    std::istream&        is_;
+    rtac::ChunkHeader32& header_;
+
+    DeserializeStub32(std::istream& is, ChunkHeader32& header) :
+        is_(is), 
+        header_(header)
+    {
+        is_.read((char*)&header_, sizeof(ChunkHeader32));
+    }
+
+    public:
+
+    template <typename T, template<typename>class VectorT>
+    std::istream& operator>>(VectorT<T>& out) {
+        out.resize(header_.size / sizeof(T));
+        unsigned int remainer = header_.size - out.size()*sizeof(T);
+        if(remainer != 0) {
+            std::cerr << "Warning : serialized data size is not a multiple of "
+                      << "requested type size.\n";
+        }
+        is_.read((char*)out.data(), sizeof(T)*out.size());
+        is_.ignore(remainer + header_.padding());
+
+        return is_;
+    }
+};
+/**
+ * This is a temporary type automatically instanciated when using operator>> on
+ * a std::ostream and an empty ChunkHeader.
+ */
+class DeserializeStub64
+{
+    public:
+    
+    //restricting this object instanciation to this function
+    friend DeserializeStub64 (::operator>>)(std::istream&, rtac::ChunkHeader64&);
+
+    protected:
+
+    std::istream&        is_;
+    rtac::ChunkHeader64& header_;
+
+    DeserializeStub64(std::istream& is, ChunkHeader64& header) :
+        is_(is), 
+        header_(header)
+    {
+        is_.read((char*)&header_, sizeof(ChunkHeader64));
+    }
+
+    public:
+
+    template <typename T, template<typename>class VectorT>
+    std::istream& operator>>(VectorT<T>& out) {
+        out.resize(header_.size / sizeof(T));
+        unsigned int remainer = header_.size - out.size()*sizeof(T);
+        if(remainer != 0) {
+            std::cerr << "Warning : serialized data size is not a multiple of "
+                      << "requested type size.\n";
+        }
+        is_.read((char*)out.data(), sizeof(T)*out.size());
+        is_.ignore(remainer + header_.padding());
+
+        return is_;
+    }
+};
+
 } //namespace rtac
 
 
@@ -137,6 +222,16 @@ inline rtac::SerializeStub operator<<(std::ostream& os, const rtac::ChunkHeader3
 inline rtac::SerializeStub operator<<(std::ostream& os, const rtac::ChunkHeader64& chunkHeader)
 {
     return rtac::SerializeStub(os, chunkHeader);
+}
+
+inline rtac::DeserializeStub32 operator>>(std::istream& is, rtac::ChunkHeader32& chunkHeader)
+{
+    return rtac::DeserializeStub32(is, chunkHeader);
+}
+
+inline rtac::DeserializeStub64 operator>>(std::istream& is, rtac::ChunkHeader64& chunkHeader)
+{
+    return rtac::DeserializeStub64(is, chunkHeader);
 }
 
 #endif //_DEF_RTAC_BASE_SERIALIZATION_SERIALIZATION_H_
--- tests/src/serialization.cpp
+++ tests/src/serialization.cpp
@@ -2,11 +2,14 @@
 #include <fstream>
 
 #include <rtac_base/serialization/serialization.h>
+#include <rtac_base/containers/HostVector.h>
 using namespace rtac;
 
 int main()
 {
     std::string data("abc");
+
+    std::cout << "64bit aligned serialization" << std::endl;
 
     std::ostringstream oss64;
     oss64 << ChunkHeader64("RTAC_DAT", data.size()) << data.c_str();
@@ -19,6 +22,18 @@ int main()
     }
     std::cout << std::dec << std::endl;
 
+    std::istringstream iss64(oss64.str());
+    auto header64 = ChunkHeader64::Empty();
+    HostVector<char> data64;
+    iss64 >> header64 >> data64;
+    std::cout << header64.id << " :";
+    for(auto v : data64) {
+        std::cout << ' ' << std::hex << (unsigned int)v;
+    }
+    std::cout << std::endl;
+    std::cout << "stream position : " << std::dec << iss64.tellg() << std::endl;
+
+    std::cout << std::endl << "32bit aligned serialization" << std::endl;
     std::ostringstream oss32;
     oss32 << ChunkHeader32("RTAC", data.size()) << data.c_str();
     std::cout << "serialized : '" << oss32.str() << '\'' << std::endl;
@@ -29,6 +44,17 @@ int main()
         std::cout << ' ' << std::hex << (unsigned int)serialized32[i];
     }
     std::cout << std::endl;
+
+    std::istringstream iss32(oss32.str());
+    auto header32 = ChunkHeader32::Empty();
+    HostVector<char> data32;
+    iss32 >> header32 >> data32;
+    std::cout << header32.id << " :";
+    for(auto v : data32) {
+        std::cout << ' ' << std::hex << (unsigned int)v;
+    }
+    std::cout << std::endl;
+    std::cout << "stream position : " << std::dec << iss32.tellg() << std::endl;
 
     return 0;
 }
